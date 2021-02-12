260b0fdb79128e3d9cb62fe0df99c8b3540ebef9
AhmedBHameed <ahmedbazy@gmail.com>
2021-02-12 02:31:19 +0100

    Testing new deployment

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Shutdown docker container ...\n"
 {
     sudo docker-compose -f docker-compose.yml down && echo "Rebuild docker container ...\n" && sudo docker-compose -f docker-compose.yml up --build -d
 } || {
-    echo "Rebuild docker container  ...\n" && sudo docker-compose -f docker-compose.yml up --build -d
+    echo "Rebuild docker container  ...\n" && sudo docker-compose -f docker-compose.yml build && sudo docker-compose -f docker-compose.yml up -d
 }
 
 
